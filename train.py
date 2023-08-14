578ce8326b4cedf23ec3bc7872108c20f76ff13b
chiraglodaya-zebra <115172849+chiraglodaya-zebra@users.noreply.github.com>
2023-08-14 13:45:53 +0530

    Delete train.py

--- train.py
+++
@@ -1,7 +0,0 @@
-import hydra
-from omegaconf import DictConfig
-
-@hydra.main(config_path='config.yaml')
-def main(cfg: DictConfig) -> None:
-  print(cfg.model.node1) # 128
-  print(cfg.optimizer.lr) # 0.001
