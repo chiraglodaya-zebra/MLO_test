1a3be3c2ef6fb99dfec03da7c54b8d8f35162992
Chirag Lodaya <chirag.lodaya@zebra.com>
2023-09-07 15:37:48 +0530

    converting to dict

--- ANN_Latest/set_task_values.py
+++ ANN_Latest/set_task_values.py
@@ -14,7 +14,7 @@ import datetime
 import argparse
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("--task_values", type=str, required=True)
+parser.add_argument("--task_values", type=dict, required=True)
 
 args, unknown = parser.parse_known_args()
 print("Task Values", "->", args.task_values)
