622277e1e2299ee1c6f21bfec51c75c724a13eef
Chirag Lodaya <chirag.lodaya@zebra.com>
2023-09-07 19:01:20 +0530

    changes

--- ANN_Latest/get_task_values.py
+++ ANN_Latest/get_task_values.py
@@ -26,6 +26,7 @@ task_values = dbutils.jobs.taskValues.get(
 
 # task_values = task_values[1:len(task_values)-1]
 print(task_values)
+task_values = task_values.replace("'",'"')
 task_values_json = json.loads(task_values)
 
 print(task_values_json)
