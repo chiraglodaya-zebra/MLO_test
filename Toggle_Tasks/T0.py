40624e0e3b400e19df6923e8c037e032684f1fed
Chirag Lodaya <chirag.lodaya@zebra.com>
2023-09-14 00:05:14 +0530

    task value checks

--- Toggle_Tasks/T0.py
+++ Toggle_Tasks/T0.py
@@ -5,42 +5,21 @@
 import os
 import sys
 import time
-import json
-
-
+import datetime
 # ---------------------------------------------------------------------------- #
 #                                    PARAMS                                    #
 # ---------------------------------------------------------------------------- #
 
+
 import argparse
 
 parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("--task_values", type=str, required=True)
 
 args, unknown = parser.parse_known_args()
+print("Task Values", "->", args.task_values)
 
 
-task_values = dbutils.jobs.taskValues.get(
-                            taskKey = "T1",
-                            key   = "task_values")
 
-
-# task_values = task_values[1:len(task_values)-1]
-print(task_values)
-task_values = task_values.replace("'",'"')
-print(task_values)
-task_values_json = json.loads(task_values)
-print(task_values_json)
-
-all_tags = {}
-
-for tag in json.loads(spark.conf.get("spark.databricks.clusterUsageTags.clusterAllTags")):
-
-  all_tags[tag['key']] = tag['value']
-
-task_name = all_tags.get('task')
-
- 
-
-# Print the task name
-
-print("Task Name:", task_name)
+dbutils.jobs.taskValues.set(key   = "task_values", \
+                            value = args.task_values)
